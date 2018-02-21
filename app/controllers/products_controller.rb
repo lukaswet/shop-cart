47bb1801bbbfda8c854381a738405da68ae0186e
lukaswet <lukaswet@gmail.com>
2018-02-22 01:02:50 +0200

    add edit & update methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,4 +27,21 @@ class ProductsController < ApplicationController
 
     redirect_to products_path
   end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update_attributes(product_params)
+
+    redirect_to products_path
+  end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:name, :price)
+    end
 end
